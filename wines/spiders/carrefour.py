590bed308e22a734e8f12d7737639395cc522c1e
Marcos Caballero <48332217+mcaballero99@users.noreply.github.com>
2022-03-21 10:08:27 +0100

    Updated carrefour start_urls list

--- wines/spiders/carrefour.py
+++ wines/spiders/carrefour.py
@@ -11,16 +11,24 @@ name = firstWine.css("a.js-gap-product-click::text").get().replace("\n", "")
 price = firstWine.xpath("//p[contains(@class,'price price-oferta')]/text()").extract()[0].replace("\n", "")
 winery = firstWine.css('a::attr(title)').re(r"^Buscar\sproductos\spor\sla\smarca\s*(.*)")[0]
 location = firstWine.css('a::attr(title)').re(r"^D.O.\s*(.*)")[0].replace("Ca. ", "")
+# https://blog.appliedinformaticsinc.com/manipulate-scrapy-start_urls-before-a-request-is-made/
+# https://stackoverflow.com/questions/9322219/how-to-generate-the-start-urls-dynamically-in-crawling
 """
 
 class CarrefourClickSpider(scrapy.Spider):
     name = 'carrefour'
     allowed_domains = ['carrefour.es']
 
-    # https://blog.appliedinformaticsinc.com/manipulate-scrapy-start_urls-before-a-request-is-made/
-    # https://stackoverflow.com/questions/9322219/how-to-generate-the-start-urls-dynamically-in-crawling
-    start_urls = ['https://www.carrefour.es/bodega/vinos-tintos/vinos-tintos-nacionales/ver-todos/N-1dxafdm/c?No=%d&Nr%3DAND%28product.salepointWithActivePrice_004583%3A1%2Cproduct.shopCodes%3A004583%2COR%28product.siteId%3AbodegaSite%29%29OR%29' %n for n in range(0, 1042)]
-    # start_urls = ['https://www.carrefour.es/bodega/vinos-tintos/vinos-tintos-nacionales/ver-todos/N-1dxafdm/c', 'https://www.carrefour.es/bodega/vinos-blancos/vinos-blancos-nacionales/ver-todos/N-distyh/c', 'https://www.carrefour.es/bodega/vinos-rosados/vinos-rosados-nacionales/ver-todos/N-1yz8sfs/c']
+    red_urls = [
+        f'https://www.carrefour.es/bodega/vinos-tintos/vinos-tintos-nacionales/ver-todos/N-1dxafdm/c?No={n}&Nr%3DAND%28product.salepointWithActivePrice_004583%3A1%2Cproduct.shopCodes%3A004583%2COR%28product.siteId%3AbodegaSite%29%29OR%29'
+        for n in range(0, 1028, 24)]
+    white_urls = [
+        f'https://www.carrefour.es/bodega/vinos-blancos/vinos-blancos-nacionales/ver-todos/N-distyh/c?No={n}&Nr%3DAND%28product.salepointWithActivePrice_004583%3A1%2Cproduct.shopCodes%3A004583%2COR%28product.siteId%3AbodegaSite%29%29OR%29'
+        for n in range(0, 429, 24)]
+    rose_urls = [
+        f'https://www.carrefour.es/bodega/vinos-rosados/vinos-rosados-nacionales/ver-todos/N-1yz8sfs/c?No={n}&Nr%3DAND%28product.salepointWithActivePrice_004583%3A1%2Cproduct.shopCodes%3A004583%2COR%28product.siteId%3AbodegaSite%29%29OR%29'
+        for n in range(0, 80, 24)]
+    start_urls = red_urls + white_urls + rose_urls
 
     def parse(self, response):
         for link in response.css('a.js-gap-product-click::attr(href)'):
